8421bbf537788b193e818f4c4c83c84de519316f
John <johnfr14.dev@proton.me>
2023-07-27 21:50:05 +0900

    add color for printf

--- include/minishell.h
+++ include/minishell.h
@@ -2,13 +2,13 @@
 # define MINISHELL_H
 
 // Color
-#define RED     "\033[31;1m"
-#define GREEN   "\033[0;32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
+# define RED     "\033[31;1m"
+# define GREEN   "\033[0;32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define RESET   "\x1b[0m"
 
 # include "stdio.h"
 # include "unistd.h"
